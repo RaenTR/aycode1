8fe061b3e01c9a671376ea52a7c4172ed93baaf6
AYCode <43270614+RaenTR@users.noreply.github.com>
2023-06-22 12:48:44 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -20,45 +20,45 @@ export default function AwardApp({ Component, pageProps }) {
   const NavItems = [
     {
       link: true,
-      name: "Home",
+      name: "Ana Sayfa!",
       icon: "fal fa-home",
       activeIcon: "fa fa-home",
       href: "/",
     },
     {
       link: true,
-      name: "Commands",
+      name: "Komutlar!",
       icon: "fa fa-list-alt",
       activeIcon: "fa fa-list-alt",
       href: "/commands",
     },
     {
       link: true,
-      name: "Support",
+      name: "Destek!",
       icon: "fab fa-discord",
       activeIcon: "fab fa-discord",
-      href: "https://discord.gg/ZcDprCrFTn",
+      href: "https://discord.gg/eQWAEVShhH",
     },
     {
       link: true,
-      name: "Add Bot",
+      name: "Sunucuna Ekle.",
       icon: "fal fa-robot",
       activeIcon: "fab fa-robot",
-      href: "https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028603968&scope=applications.commands%20bot",
+      href: "https://discord.com/api/oauth2/authorize?client_id=1119222704441147424&permissions=8&scope=bot",
     },
        {
       link: true,
-      name: "Vote",
+      name: "Oy Ver!",
       icon: "fa fa-plus",
       activeIcon: "fa fa-plus",
-      href: "https://top.gg/bot/774043716797071371/vote",
+      href: "404.js",
     },
     {
       link: true,
-      name: "Partners",
+      name: "Partnerler",
       icon: "fal fa-handshake",
       activeIcon: "fa fa-handshake",
-      href: "/partners",
+      href: "404.js",
     }
   ]
 
@@ -71,7 +71,7 @@ export default function AwardApp({ Component, pageProps }) {
       />
       <Head>
         <title>
-          Astra Bot
+          AYCode Bot
         </title>
       </Head>
       <main className="transition-all duration-200 z-10 absolute inset-0 px-5 h-screen max-w-7xl w-full mx-auto">
