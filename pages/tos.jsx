72ac971a0316e6b05dab238c6915ce61c4e5a9ce
AYCode <43270614+RaenTR@users.noreply.github.com>
2023-06-23 19:23:58 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -1,21 +1,17 @@
 export default function Tos() {
   return (
     <div>
-     <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
-      <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
+     <h2 className="mt-4 text-white font-bold text-3xl">Hizmet şartları</h2>
+      <p className="text-sm text-white/30 mb-1">Son Güncelleme: 00/00/0000 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the Astra bot for free, it is completely prohibited to sell or market for a fee.</p>
-<div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
-<div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
-<div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
-<div className="py-3"></div>
-<p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
-<div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot at certain intervals.</p>
-</div>
-    </div>
-  );
-}
+<p>⊳ Her discord sunucusu AYCode botu ücretsiz olarak kullanabilir, ücret karşılığında satılması veya pazarlanması kesinlikle yasaktır.</p> 
+<div className="py-3"></div> 
+<p>⊳ Bot komutlarını bota zarar vermek için kullanamazsınız.</p> 
+<div className="py-3"></div> 
+<p>⊳ Diğer sunuculara zarar vermek için açık kaynak kodlarını kullanamazsınız..</p> 
+<div className="py-3"></div> 
+<p>⊳ Diğer platformlar aracılığıyla açık kaynak kodlarını satamaz veya pazarlayamazsınız..</p> 
+<div className="py-3"></div> 
+<p>⊳ Botu kullanırken diğer kullanıcılara zarar verecek şekilde troll amaçlı kullanamazsınız..</p> 
+<div className="py-3"></div> 
+<p>⊳ Belirli discord bot komutlarını kullanmak için şuna oy vermeniz gerekir: <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">AYCode</a> belirli aralıklarla bot .</p>
